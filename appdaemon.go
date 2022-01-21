d39414bb33857cf3bb9a5fce785584bcea4c08bf
shlu <shilulu@forensix.cn>
2022-01-21 17:09:16 +0800

    新增支持端口检查机制，因为发现检查进程并不可靠，当进程存在时，该app并不一定在运行了，具体应该涉及到Android系统进程优化机制了

--- appdaemon.go
+++ appdaemon.go
@@ -8,28 +8,76 @@ import (
 	"time"
 )
 
-func Start()  {
-	dcShell :=dcshell.NewDcShell(true,false)
-	if len(os.Args)!=2{
+func Start() {
+	dcShell := dcshell.NewDcShell(true, checkDebug())
+	if len(os.Args) == 2 {
+		packageNames := strings.Split(os.Args[1], "/")
+		log.Println("packageNames:", packageNames)
+		for {
+			daemonAppImpl(packageNames, dcShell)
+		}
+	} else if len(os.Args) == 3 {
+		if os.Args[1]!="-s"{
+			log.Fatalln("appdaemon -s package:port/package:port")
+			return
+		}
+		packagePorts := strings.Split(os.Args[2], "/")
+		log.Println("packagePorts:", packagePorts)
+		for {
+			daemonServerImpl(packagePorts, dcShell)
+		}
+	} else {
 		log.Fatalln("appdaemon has must arg")
 		return
 	}
-	packageNames:=strings.Split(os.Args[1],"/")
-	log.Println("package:",packageNames)
-	for{
-		daemonImpl(packageNames, dcShell)
-	}
+
 }
-func daemonImpl(packages []string,shell *dcshell.DcShell) {
+
+func checkDebug() bool {
+	for _, env := range os.Environ() {
+		if strings.Contains(env,"="){
+			key:=strings.Trim(strings.Split(env,"=")[0]," ")
+			value:=strings.Trim(strings.Split(env,"=")[1]," ")
+			if strings.Contains(strings.ToUpper(key),"ADDEBUG"){
+				if strings.Contains(strings.ToLower(value),"true"){
+					return true
+				}else {
+					return false
+				}
+			}
+		}
+
+	}
+	return false
+}
+func daemonAppImpl(packages []string, shell *dcshell.DcShell) {
 	defer func() {
-		if err:=recover();err!=nil{
-			log.Println("daemonImpl.err:",err.(error).Error())
+		if err := recover(); err != nil {
+			log.Println("daemonAppImpl.err:", err.(error).Error())
 		}
 	}()
 	for _, pkgName := range packages {
-		if !shell.LaunchApp(pkgName){
-			log.Println("launch app failed:",pkgName)
+		if !shell.LaunchAppWhenStopped(pkgName) {
+			log.Println("launch app failed:", pkgName)
 		}
 	}
-	time.Sleep(100*time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
+}
+func daemonServerImpl(packagePorts []string, shell *dcshell.DcShell) {
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("daemonServerImpl.err:", err.(error).Error())
+		}
+	}()
+	for _, packagePort := range packagePorts {
+		pkg:=strings.Split(packagePort,":")[0]
+		port:=strings.Split(packagePort,":")[1]
+		if !shell.CheckPortIsListen(port){
+			if !shell.LaunchApp(pkg) {
+				log.Println("launch app failed:", pkg)
+			}
+		}
+
+	}
+	time.Sleep(100 * time.Millisecond)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module appdaemon
 
 go 1.16
 
-require github.com/Humenger/go-devcommon v0.0.0-20220119060944-472cbe8f4b34 // indirect
+require github.com/Humenger/go-devcommon v0.0.0-20220121073204-83af80eccce1
